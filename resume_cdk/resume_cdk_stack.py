84a3b4bca35f6cf678e56e112e8ffc6863a9d2d9
Michael Svajhart <msvajhart@outlook.com>
2025-08-01 17:30:00 -0400

    adding some test rest api's

--- resume_cdk/resume_cdk_stack.py
+++ resume_cdk/resume_cdk_stack.py
@@ -5,6 +5,8 @@ from aws_cdk import (
     aws_cloudfront_origins as origins,
     aws_certificatemanager as certificatemanger,
     aws_iam as iam,
+    aws_apigateway as apigateway,
+    aws_lambda as _lambda,
     RemovalPolicy,
     Duration
 )
@@ -68,4 +70,48 @@ class ResumeCdkStack(Stack):
                 }
             )
         )
+
         
+        api = apigateway.RestApi(
+            self, f"{stack_name}-RestApi",
+            rest_api_name=f"{stack_name}-RestApi",
+            description="This is my sample API built with CDK.",
+        )
+        
+         # /hello route
+        hello = api.root.add_resource("hello")
+
+         # Lambda function
+        hello_lambda = _lambda.Function(
+            self, "HelloLambdaHandler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="hello.handler",
+            code=_lambda.Code.from_asset("lambda"),
+        )
+
+        # Lambda integration
+        lambda_integration = apigateway.LambdaIntegration(
+            hello_lambda,
+            request_templates={"application/json": '{ "statusCode": "200" }'}
+        )
+
+        hello.add_method("GET", lambda_integration)
+
+         # /test route
+        test = api.root.add_resource("test")
+
+         # Lambda function
+        test_lambda = _lambda.Function(
+            self, "TestingoLambdaHandler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="testing.handler",
+            code=_lambda.Code.from_asset("lambda"),
+        )
+
+        # Lambda integration
+        test_integration = apigateway.LambdaIntegration(
+            test_lambda,
+            request_templates={"application/json": '{ "statusCode": "200" }'}
+        )
+
+        test.add_method("GET", test_integration)
